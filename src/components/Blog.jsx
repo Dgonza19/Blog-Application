073ebc799005983586006868c41b08a5f0afe684
Daisygu19 <daisyg@mikenieva.com>
2025-03-12 11:28:32 -0400

    Initial Commit

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,10 +2,6 @@ import React from "react";
 
 const Blog = () => {
 
-    const deletePost = (id) => {
-        setPosts(posts.filter(post => post.id !== id))
-    };
-
     return(
         <div>
             <h1>Bienvenidos al Blog!</h1>
